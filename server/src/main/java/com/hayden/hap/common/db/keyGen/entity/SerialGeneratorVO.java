b719e73e265b900cc9a48aeeb0dbb39c8c9f5d56
xudongcheng <xudongcheng@ushayden.com>
2025-11-17 14:49:01 +0800

    delete some error

--- server/src/main/java/com/hayden/hap/common/db/keyGen/entity/SerialGeneratorVO.java
+++ server/src/main/java/com/hayden/hap/common/db/keyGen/entity/SerialGeneratorVO.java
@@ -1,69 +1,26 @@
 package com.hayden.hap.common.db.keyGen.entity;
 
 import com.hayden.hap.common.common.entity.BaseVO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-/** 
- * 
- * @ClassName: SerialGeneratorVO 
- * @Description: 序列号生成器实体类
- * @author LUYANYING
- * @date 2015年5月27日 下午3:02:26 
- * @version V1.0   
- *
- */
-public class SerialGeneratorVO extends BaseVO{
-	/**
-	 * serialVersionUID:TODO().
-	 */
-	private static final long serialVersionUID = 1L;
-	private Long serialgenid;
-	private String gencode;
-	private String gennext;
-	private Long gencache;
-	private String genprefix;
-	private long counter;
-	
-	public SerialGeneratorVO(){
-		super("SY_SERIAL_GENERATOR");
-	}
-	
-	public String getGencode() {
-		return gencode;
-	}
-	public void setGencode(String gencode) {
-		this.gencode = gencode;
-	}
-	public String getGennext() {
-		return gennext;
-	}
-	public void setGennext(String gennext) {
-		this.gennext = gennext;
-	}
-	public String getGenprefix() {
-		return genprefix;
-	}
-	public void setGenprefix(String genprefix) {
-		this.genprefix = genprefix;
-	}
-	public Long getGencache() {
-		return gencache;
-	}
-	public void setGencache(Long gencache) {
-		this.gencache = gencache;
-	}
-	
-	public long getCounter() {
-		return counter;
-	}
-	public void setCounter(long counter) {
-		this.counter = counter;
-	}
 
-	public Long getSerialgenid() {
-		return serialgenid;
-	}
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SerialGeneratorVO extends BaseVO {
+    /**
+     * serialVersionUID:TODO().
+     */
+    private static final long serialVersionUID = 1L;
+    private Long serialgenid;
+    private String gencode;
+    private String gennext;
+    private Long gencache;
+    private String genprefix;
+    private long counter;
 
-	public void setSerialgenid(Long serialgenid) {
-		this.serialgenid = serialgenid;
-	}
+    public SerialGeneratorVO() {
+        super("SY_SERIAL_GENERATOR");
+    }
+
 }
